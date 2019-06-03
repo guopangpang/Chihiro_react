122c326d74ef95a1230d170ce8e9359ca1321324
guocheng <guocheng@inspure.com>
2019-06-03 10:17:24 +0800

    Updates

---
+++ precache-manifest.df7400f498e41770cbc1164a759217f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9a4ed482a5dc59948be7d89f064f7b",
+    "url": "/text/index.html"
+  },
+  {
+    "revision": "4f35400bacca27e3fc7b",
+    "url": "/text/static/css/main.df50d47d.chunk.css"
+  },
+  {
+    "revision": "eab1aa1acbd3a4be3ed4",
+    "url": "/text/static/js/2.3d303779.chunk.js"
+  },
+  {
+    "revision": "4f35400bacca27e3fc7b",
+    "url": "/text/static/js/main.5f7a9274.chunk.js"
+  },
+  {
+    "revision": "1563eaa2b253bc7688b9",
+    "url": "/text/static/js/runtime~main.cf280aaf.js"
+  },
+  {
+    "revision": "2699f75413772c234d60ecaf5ec935f4",
+    "url": "/text/static/media/image.2699f754.jpg"
+  }
+]);
