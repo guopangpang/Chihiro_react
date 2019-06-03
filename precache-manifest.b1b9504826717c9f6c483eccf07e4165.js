6256fa3e77fdda8df5786d5f7eb51609c740ebf2
guocheng <guocheng@inspure.com>
2019-06-03 15:23:32 +0800

    Updates

---
+++ precache-manifest.b1b9504826717c9f6c483eccf07e4165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275359edbab7325aeccac35e3fea2aef",
+    "url": "/chihiroreact/index.html"
+  },
+  {
+    "revision": "a6a7ad92b3e8d01391bd",
+    "url": "/chihiroreact/static/css/main.df50d47d.chunk.css"
+  },
+  {
+    "revision": "eab1aa1acbd3a4be3ed4",
+    "url": "/chihiroreact/static/js/2.3d303779.chunk.js"
+  },
+  {
+    "revision": "a6a7ad92b3e8d01391bd",
+    "url": "/chihiroreact/static/js/main.d4666a94.chunk.js"
+  },
+  {
+    "revision": "1c3e126e9d62a62f13b5",
+    "url": "/chihiroreact/static/js/runtime~main.a5e85ab0.js"
+  },
+  {
+    "revision": "2699f75413772c234d60ecaf5ec935f4",
+    "url": "/chihiroreact/static/media/image.2699f754.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./chihiroreact/precache-manifest.d343bd90110b115bd31f94488316ee0e.js"
+  "/chihiroreact/precache-manifest.b1b9504826717c9f6c483eccf07e4165.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./chihiroreact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chihiroreact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
