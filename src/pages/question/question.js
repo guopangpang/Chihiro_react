3d2b4a515b3c1980cde774cad140a20f2de4919a
guocheng <guocheng@inspure.com>
2019-06-11 17:52:00 +0800

    create Chihiro react

--- src/pages/question/question.js
+++ src/pages/question/question.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { InfiniteLoader, List } from 'react-virtualized';
 import './question.scss'
 
 import Header from '../../components/header/header'
@@ -12,7 +13,15 @@ class Question extends Component{
             question_list:[]
         }
     }
+    componentWillMount(){
+        this.get_data();
+    }
+
     componentDidMount(){
+        console.log(22);
+
+    }
+    get_data = () =>{
         let obj = {
             title:'小锅米线加盟有什么流程？',
             info:'我想要加盟一下云南十里香小锅米线，但是有什么\n' +
@@ -24,22 +33,58 @@ class Question extends Component{
             header_img:'../../image/image2.jpg',
             img:'../../image/image2.jpg'
         };
-        for(let i=0;i<6;i++){
+        for(let i=0;i<20;i++){
             this.state.question_list.push(obj)
         }
         this.setState({
             question_list:this.state.question_list
         });
+    };
+    rowRenderer  = ({ key, index}) => {
+        return (
+            <HomeItem key={key} item={this.state.question_list[index]} history={this.props.history}/>
+        )
     }
+    onScroll = () => {
+        console.log('onScroll');
+        this.get_data();
+    };
+    isRowLoaded = (index) =>{
+        return !!this.state.question_list[index]
+    };
     render(){
         return(
             <div className={'question_container'}>
                 <Header/>
-                {
-                    this.state.question_list.map((item,index)=>{
-                        return <HomeItem key={index} item={item} history={this.props.history}/>
-                    })
-                }
+                <InfiniteLoader
+                    isRowLoaded={this.isRowLoaded}
+                    loadMoreRows={this.onScroll}
+                    rowCount={10}
+                    threshold={5}
+                >
+                    {({ onRowsRendered, registerChild }) => (
+                        <List
+                            height={200}
+                            onRowsRendered={onRowsRendered}
+                            ref={registerChild}
+                            rowCount={100}
+                            rowHeight={20}
+                            rowRenderer={this.rowRenderer}
+                            width={300}
+                        />
+                    )}
+
+                    {/*{({})=>(*/}
+                        {/*this.state.question_list.map((item,index)=>{*/}
+                            {/*return <HomeItem key={index} item={item} history={this.props.history}/>*/}
+                        {/*})*/}
+                    {/*)}*/}
+                </InfiniteLoader>
+                {/*{*/}
+                    {/*this.state.question_list.map((item,index)=>{*/}
+                        {/*return <HomeItem key={index} item={item} history={this.props.history}/>*/}
+                    {/*})*/}
+                {/*}*/}
                 <Footer home_props={this.props} />
             </div>
         )
