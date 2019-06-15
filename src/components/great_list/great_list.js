578f9e0bba53358433344855bcba2403ac07704e
guocheng <guocheng@inspure.com>
2019-06-15 16:34:23 +0800

    create Chihiro react

--- src/components/great_list/great_list.js
+++ src/components/great_list/great_list.js
@@ -6,7 +6,13 @@ class GreatList extends Component{
         super(props);
         this.state = {
             great_list:''
-        }
+        };
+        this.show_and_buffer = '';
+        this.startOffset = 0;
+        this.endOffset = 0;
+        this.show_list = [];
+        this.height = '';
+
     }
     componentDidMount(){
         console.log(this.props)
@@ -15,24 +21,25 @@ class GreatList extends Component{
         let scrollHeight = this.state.great_list.scrollHeight;
         let offsetHeight = this.state.great_list.offsetHeight;
         let scrollTop = this.state.great_list.scrollTop;
-        console.log(scrollHeight,offsetHeight,scrollTop);
-        if((scrollHeight - offsetHeight - scrollTop)<100){
+        // console.log(scrollHeight,offsetHeight,scrollTop);
+        if((scrollHeight - offsetHeight - scrollTop)=== 0){
+            // alert(2)
             this.props.onScroll()
         }
     };
     render() {
-        let show_and_buffer = this.props.show_length + this.props.buffersize;
-        let startOffset = 0, endOffset = 0;
-        let show_list = this.props.list.slice(this.props.start - show_and_buffer, this.props.start);;
-        let height = this.props.height.split('rem')[0]*this.props.list.length;
-        startOffset = height - 68;
-        console.log(height);
+        this.show_and_buffer = this.props.show_length + this.props.buffersize;
+        this.show_list = this.props.list.slice(this.props.start - this.show_and_buffer, this.props.start);
+        this.height = this.props.height.split('rem')[0]*this.props.list.length;
+
+        this.startOffset = this.height - 68;
+        console.log(this.startOffset);
 
         return (
             <div className={'great_list_container'} onScrollCapture={this.onScroll} ref={(great_list)=>{this.state.great_list= great_list}}>
-                <div style={{'height':height - startOffset + 'rem', 'paddingTop':startOffset + 'rem'}} >
+                <div style={{'height':this.height - this.startOffset + 'rem', 'paddingTop':this.startOffset + 'rem'}} >
                     {
-                        show_list.map((item,index)=>{
+                        this.show_list.map((item,index)=>{
                             return <this.props.item key={index} item={item}/>
                         })
                     }
